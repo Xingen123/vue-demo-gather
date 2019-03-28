a1f08fb4f30819dcb2e44dc207ead0b74570cbed
xingen123 <335728491@qq.com>
2019-03-28 14:34:31 +0800

    xe

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+
+    <div class="tab" >
+        <div v-for="(item,index) in tabs" :key="index" @click="tabList(item)" :class="{active:currentTab == item}">
+          {{item}}
+        </div>
+    </div>
+
+
+    <component class="box" :is="currentTab"></component>
+  </div>
+</template>
+
+<script>
+
+import svgDemo from './demo1'
+
+export default {
+  components:{
+			svgDemo
+  },
+  data () {
+    return {
+      currentTab: 'svgDemo',
+      tabs:["svgDemo","demo_2","demo_3","demo_4"]
+    }
+  },
+  methods:{
+    tabList(item){
+      this.currentTab = item
+    }
+  },
+  mounted(){
+
+  }
+
+}
+</script>
+<style scoped>
+.hello{
+  display: flex;
+  flex-direction: row;
+}
+.tab>div{
+  margin: 10px;
+}
+.active{
+  font-weight: bold;
+}
+</style>
---
+++ src/components/demo1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+      <svg width="100%" height="100%" >
+
+      <polyline points="0,0 200," style="fill:white;stroke:red;"/>
+      </svg>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+  mounted(){
+
+  }
+
+}
+</script>
+<style scoped>
+
+</style>
